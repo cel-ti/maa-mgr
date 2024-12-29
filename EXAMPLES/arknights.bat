7b5e626cc64e01d59e0b56e2c96d8f944d2802ee
cel-ti <96926560+cel-ti@users.noreply.github.com>
2024-12-28 22:17:00 -0800

    Update arknights.bat

--- EXAMPLES/arknights.bat
+++ EXAMPLES/arknights.bat
@@ -1,3 +1,6 @@
 reldplay --name p1 launch
 maamgr maa patch -p "config/gui.json" "Configurations/Default/Start.RunDirectly=True"
 maa-arknights
+timeout /t 2400
+reldplay --name p1 killapp -p com.hypergryph.arknights.bilibili
+taskkill /f /fi "WINDOWTITLE eq MAA*"
