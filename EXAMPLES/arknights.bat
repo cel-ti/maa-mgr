170d4eab29465f1ad26ab1c7e5e096662dfdcda2
cel-ti <96926560+cel-ti@users.noreply.github.com>
2024-12-28 22:33:27 -0800

    script: add timeout

--- EXAMPLES/arknights.bat
+++ EXAMPLES/arknights.bat
@@ -1,5 +1,6 @@
 reldplay --name p1 launch
 maamgr maa patch -p "config/gui.json" "Configurations/Default/Start.RunDirectly=True"
+timeout /t 10
 maa-arknights
 timeout /t 2400
 reldplay --name p1 killapp -p com.hypergryph.arknights.bilibili
